e5b8692faac0a8858432765759eb234f931ede05
tzioMelody <georgia.antoniou27@gmail.com>
2026-08-13 15:55:26 +0300

    Add project files.

---
+++ ClientInformationWin.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EuroPromotionProject
+{
+    /// <summary>
+    /// Interaction logic for ClientInformationWin.xaml
+    /// </summary>
+    public partial class ClientInformationWin : Window
+    {
+        public ClientInformationWin()
+        {
+            InitializeComponent();
+        }
+
+        private void PromoterCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void PresentationCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SalesPersonCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
