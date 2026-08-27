b2530044a754fbae3c6b5b92074ed9ba155d21ed
tzioMelody <georgia.antoniou27@gmail.com>
2026-08-27 14:27:39 +0300

    test error

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,31 @@ namespace EuroPromotionProject
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            this.DispatcherUnhandledException += (s, args) =>
+            {
+                MessageBox.Show(
+                    "Σφάλμα κατά την εκκίνηση:\n\n" + args.Exception.ToString(),
+                    "Unhandled Exception",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                args.Handled = true;
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (s, args) =>
+            {
+                var ex = args.ExceptionObject as Exception;
+                MessageBox.Show(
+                    "Κρίσιμο σφάλμα:\n\n" + ex?.ToString(),
+                    "Fatal Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            };
+        }
+    }
 }
+
+
