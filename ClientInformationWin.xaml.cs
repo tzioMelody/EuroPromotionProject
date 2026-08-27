939b98509b605a8927045039e0063217cdc17363
tzioMelody <georgia.antoniou27@gmail.com>
2026-08-27 14:38:40 +0300

    make comment a message

--- ClientInformationWin.xaml.cs
+++ ClientInformationWin.xaml.cs
@@ -80,7 +80,7 @@ namespace EuroPromotionProject
             Stylus.SetIsPressAndHoldEnabled(SignCanvas, false);
 
             string filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Ονόματα έκθεσης Οκτώβρη.xlsx");
-            MessageBox.Show("Ψάχνω το αρχείο εδώ: " + filePath + "\nΥπάρχει; " + File.Exists(filePath));
+            //MessageBox.Show("Ψάχνω το αρχείο εδώ: " + filePath + "\nΥπάρχει; " + File.Exists(filePath));
 
             DataTable dt = ImportExceltoDatatable(filePath);
             var itemsListPP = dt.AsEnumerable().Select(row => row["Presentation / Promotion"]?.ToString()).Where(text => !string.IsNullOrEmpty(text)).ToList();
